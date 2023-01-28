f78f0d5034633bcdd31f0384547f2092efe85959
Matushan Yogaraj <dev@matushan.com>
2023-01-28 13:04:13 +0000

    Create modal to add cryptocurrency wallets

--- reactnative/screens/crypto/CryptoWallet.js
+++ reactnative/screens/crypto/CryptoWallet.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import {Button, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View, Image} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Pressable,
+  Modal
+} from 'react-native';
 
 import data from './wallet_data.json' // Test Data
+import WalletModal from "./WalletModal";
 
 
 export default function CryptoWallet() {
+
+  const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <SafeAreaView style={styles.cryptoWallet}>
@@ -13,14 +27,17 @@ export default function CryptoWallet() {
 
         <StatusBar style="auto" />
 
-        <View style={{ flexDirection:'row', alignItems: 'center' }}>
-          <View style={{ flex: 1, flexDirection: 'column' }}>
+        <View style={{ flexDirection:'row', alignItems: 'center', backgroundColor: 'black' }}>
+          <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
             <Text style={styles.backArrow}>←</Text>
             <Text style={styles.cryptoWalletTitle}>Crypto Wallets</Text>
           </View>
         </View>
 
-        <Button title="Add Wallet" />
+        <WalletModal visible={modalVisible} setVisible={setModalVisible} />
+
+        <Button title="Add Wallet" onPress={() => setModalVisible(true)} />
+
 
         <FlatList
           style={styles.walletList}
@@ -39,7 +56,7 @@ function WalletAsset(props) {
   return (
     <View style={styles.walletAsset}>
       <View style={{ flex: 1, flexDirection:'row', alignItems: 'center' }}>
-        <View style={{ backgroundColor: 'white', padding: 5, borderRadius: 10, marginRight: 10}}>
+        <View style={{  padding: 5, borderRadius: 10, marginRight: 10}}>
           <Image style={styles.walletAssetImage} source={{ uri: `https://cryptoicons.org/api/color/${props.item.symbol.toLowerCase()}/200` }} />
         </View>
         <Text style={styles.walletAssetTitle}>{props.item.name}</Text>
@@ -54,37 +71,43 @@ function WalletAsset(props) {
 const styles = StyleSheet.create({
   cryptoWallet: {
     flex: 1,
-    paddingVertical: 300,
   },
   cryptoWalletTitle: {
     fontWeight: '900',
     fontSize: 40,
     alignSelf: 'center',
+    paddingVertical: 10,
+    color: 'white',
   },
   backArrow: {
     fontWeight: '900',
-    fontSize: 40,
+    fontSize: 30,
     position: 'absolute',
-    paddingLeft: 10,
+    marginLeft: 10,
+    color: 'white',
   },
   walletList: {
-    borderRadius: 15,
+    //borderRadius: 15,
     marginHorizontal: 10,
     marginVertical: 30,
+
   },
   walletAsset: {
     backgroundColor: '#e5e5e5',
     padding: 10,
-    borderBottomColor: '#a3a3a3',
-    borderBottomWidth: 1,
+    marginVertical: 5,
+    borderRadius: 15,
+    //borderBottomColor: '#a3a3a3',
+    //borderBottomWidth: 1,
     flexDirection:'row',
-    justifyContent:'space-between'
+    justifyContent:'space-between',
+
   },
   walletAssetTitle: {
     fontWeight: '700',
   },
   walletAssetImage: {
-    width: 32,
-    height: 32,
+    width: 50,
+    height: 50,
   }
 });
---
+++ reactnative/screens/crypto/WalletModal.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Button, Modal, Pressable, StyleSheet, Text, TextInput, View} from "react-native";
+
+export default function WalletModal(props) {
+  return (
+    <Modal
+      animationType='slide'
+      presentationStyle='pageSheet'
+      visible={props.visible}>
+      <View style={{ paddingTop: 30 }}>
+
+        <View style={{ flexDirection:'row', alignItems: 'center' }}>
+          <View style={{ flex: 1, flexDirection: 'column' }}>
+            <Pressable onPress={() => props.setVisible(false)}>
+              <Text style={styles.backArrow}>←</Text>
+            </Pressable>
+            <Text style={styles.title}>Add Wallet</Text>
+          </View>
+        </View>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Wallet Address" />
+
+        <Button title="Submit" />
+
+      </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: '900',
+    fontSize: 40,
+    alignSelf: 'center',
+  },
+  backArrow: {
+    fontWeight: '900',
+    fontSize: 40,
+    position: 'absolute',
+    paddingLeft: 10,
+  },
+  input: {
+    backgroundColor: '#e5e5e5',
+    borderRadius: 10,
+    margin: 30,
+    padding: 10,
+  }
+});
