fac1d62beee483b8bce2c1c9d324ed347607973c
krishna <krishnaprasannakumar21@gmail.com>
2023-01-29 13:45:36 +0000

    Add Basic Pie Chart

--- reactnative/App.js
+++ reactnative/App.js
@@ -1,12 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+
+import data from "./data.json"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <View>
+      <PieChart
+  data={data}
+  width={Dimensions.get("window").width}
+  height={500}
+  chartConfig={{
+    backgroundColor: "#e26a00",
+    backgroundGradientFrom: "#fb8c00",
+    backgroundGradientTo: "#ffa726",
+    decimalPlaces: 2, // optional, defaults to 2dp
+    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    style: {
+      borderRadius: 16
+    },
+    propsForDots: {
+      r: "6",
+      strokeWidth: "2",
+      stroke: "#ffa726"
+    }
+  }}
+  accessor={"amount"}
+  backgroundColor={"transparent"}
+  paddingLeft={"15"}
+  center={[10, 50]}
+  absolute
+/>
+</View>
     </View>
   );
 }
