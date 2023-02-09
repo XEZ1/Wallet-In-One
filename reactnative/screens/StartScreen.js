5c3d399304328106ee9c1ecea018e564b4d51006
Abbas Bin Vakas <abbasbinvakas@yahoo.com>
2023-02-09 11:46:25 +0000

    Add background image

--- reactnative/screens/StartScreen.js
+++ reactnative/screens/StartScreen.js
@@ -1,29 +1,34 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight, ImageBackground } from 'react-native';
 
-export default function StartScreen({ navigation }) {
+export default function StartScreen ({ navigation }) {
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <TouchableHighlight style={styles.button}> 
-        <Button title="Sign Up" onPress={() => navigation.navigate('Sign Up')} />
-      </TouchableHighlight>
-      <TouchableHighlight style={styles.button}> 
-        <Button title="Login" onPress={() => navigation.navigate('Login')} />
-      </TouchableHighlight>
-    </View>
-  );
+    return (
+      <ImageBackground
+          source={require('reactnative/assets/background.png')}
+          style={styles.background}
+      >
+        <View>
+          <StatusBar style="auto" />
+          <TouchableHighlight style={styles.button}> 
+            <Button title="Sign Up" onPress={() => navigation.navigate('Sign Up')} />
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button}> 
+            <Button title="Login" onPress={() => navigation.navigate('Login')} />
+          </TouchableHighlight>
+        </View>
+      </ImageBackground>
+    );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   button:{
     margin:10,
+  },
+  background: {
+    width: '100%',
+    height: '100%'
   }
 });
