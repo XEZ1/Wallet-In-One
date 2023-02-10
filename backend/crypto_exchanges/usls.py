77bdc58b849aa6ccdc543d933052c3b8eda8a2f1
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-10 19:40:35 +0000

    Create a url

--- backend/crypto_exchanges/usls.py
+++ backend/crypto_exchanges/usls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from crypto_exchanges import views
 
 urlpatterns = [
-#    path('', views.)
+    path('', views.BinanceAPI.get.as_view(), name='add-crypto_excahnge_wallet')
 ]
