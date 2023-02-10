aaa5f91fc57476d4a56842af2f02062a7feae397
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-10 19:51:48 +0000

    Refactor models and add binance_account model

--- backend/crypto_exchanges/models.py
+++ backend/crypto_exchanges/models.py
@@ -3,7 +3,12 @@ from accounts.models import User
 
 # Create your models here.
 
+class BinanceAccount(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    access_token = models.CharField(max_length=100)
+
 class Token(models.Model):
+    account = models.ForeignKey(BinanceAccount, on_delete=models.CASCADE)
     asset = models.CharField(max_length=50)
     free = models.FloatField()
     locked = models.FloatField()
