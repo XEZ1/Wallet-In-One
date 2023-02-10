07e60528afc77a00f0cbbcf8755d18e20ec92e89
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-10 19:53:31 +0000

    Refactor the view

--- backend/crypto_exchanges/views.py
+++ backend/crypto_exchanges/views.py
@@ -12,13 +12,17 @@ from crypto_exchanges.services import BinanceFetcher
 # Create your views here.
 
 class BinanceAPI(views.APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, format=None):
-        token = request.query_params.get("token")
-        if not token:
-            return Response({"error": "Token is required."})
+        user = request.user
+        try:
+            binance_account = BinanceAccount.objects.get(user=user)
+        except BinanceAccount.DoesNotExist:
+            return Response({"error": "Binance account is not connected."})
 
         try:
-            tokens = BinanceFetcher.fetch_tokens(token)
+            tokens = BinanceFetcher.fetch_tokens(binance_account.access_token)
             serializer = TokenSerializer(tokens, many=True)
             return Response(serializer.data)
         except Exception as e:
