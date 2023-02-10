bc94f91ed073620834f5317cb5c1e204040d198c
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-10 19:34:37 +0000

    Create a fetcher

---
+++ backend/crypto_exchanges/services.py
@@ -0,0 +1,26 @@
+from crypto_exchanges.models import Token
+import requests
+
+class BinanceFetcher:
+    @staticmethod
+    def fetch_tokens(token):
+        endpoint = "https://api.binance.com/api/v3/account"
+        headers = {
+            "X-MBX-APIKEY": token
+        }
+
+        response = requests.get(endpoint, headers=headers)
+        if response.status_code != 200:
+            raise Exception("Failed to retrieve data from Binance API.")
+
+        data = response.json()
+        balances = data["balances"]
+        tokens = []
+        for balance in balances:
+            if float(balance["free"]) > 0:
+                tokens.append(Token(
+                    asset=balance["asset"],
+                    free=balance["free"],
+                    locked=balance["locked"]
+                ))
+        return tokens
--- backend/crypto_exchanges/usls.py
+++ backend/crypto_exchanges/usls.py
@@ -1,3 +1,6 @@
 from django.urls import path
 from crypto_exchanges import views
 
+urlpatterns = [
+#    path('', views.)
+]
