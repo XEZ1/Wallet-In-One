5b50c7c342d9ae21f76703c3decc7ed88d036c65
krishna <krishnaprasannakumar21@gmail.com>
2023-02-11 14:59:58 +0000

    Update Package.json for victory charts

--- reactnative/tests/PieChart.test.js
+++ reactnative/tests/PieChart.test.js
@@ -9,4 +9,12 @@ describe('<PieChartWallet />', () => {
         const tree = renderer.create(<PieChartWallet />).toJSON();
         expect(tree).toMatchSnapshot();
       });
+
+      it('displays the text "Wallet-In-One"', () => {
+        const { getByText } = render(<PieChartWallet />);
+        const walletInOne = getByText('Wallet-In-One');
+    
+        expect(walletInOne).toBeDefined();
+        expect(walletInOne.props.children).toEqual('Wallet-In-One');
+      });
   });
