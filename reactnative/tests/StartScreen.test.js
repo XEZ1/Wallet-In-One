a8f256ab33f3947fc4364fcbc056fa4e3a48b350
krishna <krishnaprasannakumar21@gmail.com>
2023-02-12 17:34:53 +0000

    Create Initial Tests

---
+++ reactnative/tests/StartScreen.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import StartScreen from '../screens/StartScreen';
+import renderer from 'react-test-renderer';
+
+describe('<StartScreen />', () => {
+    
+})
