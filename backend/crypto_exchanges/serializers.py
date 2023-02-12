9734b1d0f9ad4da35d69c5974d2f55541109793d
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-12 04:22:14 +0000

    Create a serialiser for binance_account

--- backend/crypto_exchanges/serializers.py
+++ backend/crypto_exchanges/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from accounts.models import User
-from crypto_exchanges.models import Token
+from crypto_exchanges.models import Token, BinanceAccount
 from rest_framework.fields import CurrentUserDefault
 from crypto_exchanges.services import BinanceFetcher
 
+class BinanceAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BinanceAccount
+        fields = ('user', 'api_key', 'secret_key', 'created_at')
 class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
