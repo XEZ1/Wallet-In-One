2b1c15baf18ba96919ff2c3d62eefeab99b347ea
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-12 04:24:13 +0000

    Add a post method in the view

--- backend/crypto_exchanges/views.py
+++ backend/crypto_exchanges/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from crypto_exchanges.models import Token
-from crypto_exchanges.serializers import TokenSerializer
-from crypto_exchanges.services import BinanceFetcher
+from crypto_exchanges.serializers import TokenSerializer, BinanceAccountSerializer
+from crypto_exchanges.services import BinanceFetcher, BinanceService
 from crypto_exchanges.models import BinanceAccount
 
 
@@ -29,3 +29,21 @@ class BinanceAPI(APIView):
             return Response(serializer.data)
         except Exception as e:
             return Response({"error": str(e)})
+
+    def post(self, request):
+        serializer = BinanceAccountSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        api_key = serializer.validated_data['api_key']
+        secret_key = serializer.validated_data['secret_key']
+
+        # Use the provided API key and secret key to connect to the Binance API
+        service = BinanceService(api_key, secret_key)
+
+        # Get the user's account information
+        account = service.get_account()
+
+        # Save the user's Binance account information
+        BinanceAccount.objects.create(api_key=api_key, secret_key=secret_key)
+
+        # Return the account information to the user
+        return Response(account)
