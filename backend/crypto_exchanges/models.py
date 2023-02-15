e5e8d214247b20af18fb1086a3c08ea3ca710ade
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-15 11:56:43 +0000

    Create a huobi_account serialiser

--- backend/crypto_exchanges/models.py
+++ backend/crypto_exchanges/models.py
@@ -4,6 +4,12 @@ from accounts.models import User
 
 # Create your models here.
 
+class Token(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    asset = models.CharField(max_length=50, unique=True)
+    free = models.FloatField()
+    locked = models.FloatField()
+
 class BinanceAccount(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     api_key = models.CharField(max_length=255)
@@ -22,8 +28,3 @@ class CoinListAccount(models.Model):
     secret_key = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
-class Token(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    asset = models.CharField(max_length=50, unique=True)
-    free = models.FloatField()
-    locked = models.FloatField()
--- backend/crypto_exchanges/serializers.py
+++ backend/crypto_exchanges/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from crypto_exchanges.models import Token, BinanceAccount, CoinListAccount
+from crypto_exchanges.models import Token, BinanceAccount, HuobiAccount, CoinListAccount
 
 class BinanceAccountSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
@@ -13,6 +13,19 @@ class BinanceAccountSerializer(serializers.ModelSerializer):
         )
         binance_account.save()
         return binance_account
+
+class HuobiAccountSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    class Meta:
+        model = HuobiAccount
+        fields = ('user', 'api_key', 'secret_key', 'created_at')
+
+    def create(self, validated_data):
+        Huobi_account = CoinListAccount.objects.create(
+            **validated_data,
+        )
+        Huobi_account.save()
+        return Huobi_account
 
 class CoinListAccountSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
