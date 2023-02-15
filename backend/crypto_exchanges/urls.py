932686f4699025f75de246eae0bf5ad47d33824e
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-15 10:34:04 +0000

    Add coinlist to urls

--- backend/crypto_exchanges/urls.py
+++ backend/crypto_exchanges/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from crypto_exchanges import views
 
 urlpatterns = [
-    path('crypto-exchanges/binance', views.BinanceView.as_view(), name='add-crypto_exchange_wallet')
+    path('crypto-exchanges/binance', views.BinanceView.as_view(), name='binance'),
+    path('crypto-exchanges/coinlist', views.CoinListView.as_view(), name='coinlist')
 ]
