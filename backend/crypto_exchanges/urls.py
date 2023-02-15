367a2f4f700d9d95856931db9b03f3e0e42a62c1
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-15 12:21:51 +0000

    Add huobi view to urls

--- backend/crypto_exchanges/urls.py
+++ backend/crypto_exchanges/urls.py
@@ -3,5 +3,6 @@ from crypto_exchanges import views
 
 urlpatterns = [
     path('crypto-exchanges/binance', views.BinanceView.as_view(), name='binance'),
-    path('crypto-exchanges/coinlist', views.CoinListView.as_view(), name='coinlist')
+    path('crypto-exchanges/huobi', views.HuobiView.as_view(), name='huobi'),
+    path('crypto-exchanges/coinlist', views.CoinListView.as_view(), name='coinlist'),
 ]
