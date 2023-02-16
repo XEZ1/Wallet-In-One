84ea3a4e544f60e7cb842165cbb6dc2779895d1c
krishna <krishnaprasannakumar21@gmail.com>
2023-02-16 20:30:04 +0000

    Fix tests

--- reactnative/tests/HomePage.test.js
+++ reactnative/tests/HomePage.test.js
@@ -1,7 +1,7 @@
 // HomePage.test.js
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-import HomePage from '../screens/charts/PieChart';
+import HomePage from '../screens/charts/HomePage';
 import renderer from 'react-test-renderer';
 
 describe('<HomePage />', () => {
