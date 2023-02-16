367cf5e7c322bddb1c3b454ac884f0f8c7a8cc4f
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-02-16 20:12:45 +0000

    Add gateio_view to the urls

--- backend/crypto_exchanges/urls.py
+++ backend/crypto_exchanges/urls.py
@@ -4,5 +4,6 @@ from crypto_exchanges import views
 urlpatterns = [
     path('crypto-exchanges/binance', views.BinanceView.as_view(), name='binance'),
     path('crypto-exchanges/huobi', views.HuobiView.as_view(), name='huobi'),
+    path('crypto-exchanges/gateio', views.GateioView.as_view(), name='gateio'),
     path('crypto-exchanges/coinlist', views.CoinListView.as_view(), name='coinlist'),
 ]
