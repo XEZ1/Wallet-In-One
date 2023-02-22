1a0d14e1b0ba3baf1a97b4cb6101844013f68d84
JamalBoustani <boustani11@gmail.com>
2023-02-22 14:21:17 +0000

    Adding notifications for signup screen and login screen

--- reactnative/screens/LoginScreen.js
+++ reactnative/screens/LoginScreen.js
@@ -1,106 +1,135 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Button, TextInput, Alert } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  TextInput,
+  Alert,
+} from "react-native";
+import * as Notifications from "expo-notifications";
 
-import { useContext, useState } from 'react';
-import { userContext } from '../data';
+import { useContext, useState } from "react";
+import { userContext } from "../data";
 
-import { login } from '../authentication';
+import { login } from "../authentication";
 
 export default function SignUpScreen({ navigation }) {
+  const [user, setUser] = useContext(userContext);
 
-  const [ user, setUser] = useContext(userContext)
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
 
-  const [ username, setUsername ] = useState()
-  const [ password, setPassword ] = useState()
+  const [errors, setErrors] = useState({});
 
-  const [ errors, setErrors ] = useState({})
-  
   const loginHandler = async () => {
-    var response = await login(username,password,user,setUser);
-    if (response.status == "Error"){
-      Alert.alert('Error', response.body['message'])
-    }
-    else if (response.status == 400 && response.body){
-      setErrors(response.body)
+    var response = await login(username, password, user, setUser);
+    if (response.status == "Error") {
+      Alert.alert("Error", response.body["message"]);
+    } else if (response.status == 400 && response.body) {
+      setErrors(response.body);
 
-      if (response.body['non_field_errors']){
-        Alert.alert('Error', 'Login Error')
+      if (response.body["non_field_errors"]) {
+        Alert.alert("Error", "Login Error");
       }
+    } else {
+      sendNotification();
     }
-  }
+  };
+
+  const sendNotification = async () => {
+    await Notifications.requestPermissionsAsync();
+    await Notifications.presentNotificationAsync({
+      title: "Login Successful",
+      body: "You have successfully logged in.",
+      ios: { _displayInForeground: true },
+    });
+  };
 
   const inputStyle = (name) => {
-    if (name in errors || 'non_field_errors' in errors){
-      return [styles.input, styles.error]
+    if (name in errors || "non_field_errors" in errors) {
+      return [styles.input, styles.error];
     }
-    return [styles.input]
-  }
+    return [styles.input];
+  };
 
-  function ErrorMessage(props){
-    if (props.name in errors){
+  function ErrorMessage(props) {
+    if (props.name in errors) {
       return (
         <>
           {errors[props.name].map((value, index) => {
-            return <Text key={index} style={styles.errorText}>{value}</Text>
+            return (
+              <Text key={index} style={styles.errorText}>
+                {value}
+              </Text>
+            );
           })}
         </>
-      )
+      );
     }
     return null;
   }
-  
+
   return (
     <ScrollView style={styles.container}>
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
 
-      
-        <Text style={styles.text} >Username:</Text>
-        <TextInput style={inputStyle('username')} onChangeText={setUsername} testID='username'/>
-        <ErrorMessage name='username'></ErrorMessage>
+      <Text style={styles.text}>Username:</Text>
+      <TextInput
+        style={inputStyle("username")}
+        onChangeText={setUsername}
+        testID="username"
+      />
+      <ErrorMessage name="username"></ErrorMessage>
 
-        <Text style={styles.text} >Password:</Text>
-        <TextInput style={inputStyle('password')} onChangeText={setPassword} secureTextEntry={true} testID='password' />
-        <ErrorMessage name='password'></ErrorMessage>
-        <ErrorMessage name='non_field_errors'></ErrorMessage>
+      <Text style={styles.text}>Password:</Text>
+      <TextInput
+        style={inputStyle("password")}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+        testID="password"
+      />
+      <ErrorMessage name="password"></ErrorMessage>
+      <ErrorMessage name="non_field_errors"></ErrorMessage>
 
-        <View style={styles.parent}>
-          <Button style={styles.button} title="Log In" onPress={loginHandler} />
-        </View>
+      <View style={styles.parent}>
+        <Button style={styles.button} title="Log In" onPress={loginHandler} />
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    width: '100%',
+    backgroundColor: "#fff",
+    width: "100%",
     padding: 30,
   },
-  parent:{
+  parent: {
     marginTop: 20,
     flex: 1,
     width: "100%",
-    alignSelf:'flex-start',
+    alignSelf: "flex-start",
   },
-  text:{
+  text: {
     marginTop: 10,
     marginBottom: 10,
   },
   input: {
     height: 40,
-    width: '100%',
+    width: "100%",
     borderWidth: 0.5,
     padding: 10,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderRadius: 5,
   },
-  button:{
+  button: {},
+  error: {
+    borderColor: "red",
   },
-  error:{
-    borderColor: 'red',
-  },
-  errorText:{
+  errorText: {
     marginLeft: 10,
-    color: 'red',
-  }
+    color: "red",
+  },
 });
