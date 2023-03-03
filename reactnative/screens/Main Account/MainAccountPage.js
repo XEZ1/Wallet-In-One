d5993cfd261b9fc35b137c2f09cdfdb4db2c7943
YUSUF ABDULRAHMAN <yusuf.abdulrahman@kcl.ac.uk>
2023-03-03 20:34:59 +0000

    Redesign Account Page

--- reactnative/screens/Main Account/MainAccountPage.js
+++ reactnative/screens/Main Account/MainAccountPage.js
@@ -1,61 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/Ionicons';
+
+import React from 'react';
 
 
 export default function MainAccountPage({ navigation }) {
 
-    return (
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            flexDirection: 'column',
+            padding: 5,
+        },
+        row: {
+            flex: 1,
+            flexDirection: 'row',
+        },
+        box: {
+            flex: 1,
+            borderRadius: 15,
+            margin: 5,
+            flexDirection: 'column',
+            justifyContent: 'space-evenly',
+            alignItems: 'center'
+        },
+        text: { 
+            color: 'white', 
+            fontSize: 23, 
+            fontWeight: 'bold', 
+            textAlign: 'center'
+        }
+    });
 
+    return (
         <View style={styles.container}>
-            <StatusBar style="auto" />
-            <TouchableOpacity onPress={() => navigation.navigate("Bank Accounts")}>
-                <View style={styles.cards}>
-                    <Text>Bank Account(s)</Text>
-                </View>
+            {/* 3 buttons */}
+            <TouchableOpacity style={[styles.box, { backgroundColor: '#5686f2' }]} onPress={() => navigation.navigate("Bank Accounts")}>
+                <Icon style={{ color: 'white' }} name="bank" size={80} /><Text style={styles.text}>{'Bank Accounts'}</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate("Wallets")}>
-                <View style={styles.cards}>
-                    <Text>Crypto Wallet(s)</Text>
-                </View>
-            </TouchableOpacity >
-            <TouchableOpacity onPress={() => navigation.navigate("Crypto exchanges")}>
-                <View style={styles.cards}>
-                    <Text>Crypto Exchange(s)</Text>
-                </View>
+            <TouchableOpacity style={[styles.box, { backgroundColor: 'red' }]} onPress={() => navigation.navigate("Wallets")}>
+                <Icon2 style={{ color: 'white' }} name="wallet" size={80} /><Text style={styles.text}>{'Cryptocurrency'}</Text>
             </TouchableOpacity>
-            <TouchableOpacity >
-                <View style={styles.cards}>
-                    <Text>Stock Accounts(s)</Text>
-                </View>
+            <TouchableOpacity style={[styles.box, { backgroundColor: '#55a755' }]} >
+                <Icon style={{ color: 'white' }} name="line-chart" size={80} /><Text style={styles.text}>{'Stock Accounts'}</Text>
             </TouchableOpacity>
+            
+            {/* 4 buttons */}
+            {/* <View style={styles.row}>
+                <TouchableOpacity style={[styles.box,{backgroundColor: '#5686f2'}]} onPress={() => navigation.navigate("Bank Accounts")}>
+                    <Icon style={{color:'white'}} name="bank" size={100} /><Text style={styles.text}>{'Bank \nAccounts'}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.box,{backgroundColor: 'red'}]} onPress={() => navigation.navigate("Wallets")}>
+                    <Icon2 style={{color:'white'}} name="wallet" size={100} /><Text style={styles.text}>{'Crypto \nWallet'}</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.row}>
+                <TouchableOpacity style={[styles.box,{backgroundColor: '#5744d3'}]} onPress={() => navigation.navigate("Crypto exchanges")}>
+                    <Icon style={{color:'white'}} name="exchange" size={100} /><Text style={styles.text}>{'Crypto \nExchange'}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.box,{backgroundColor: '#55a755'}]} >
+                    <Icon style={{color:'white'}} name="line-chart" size={100} /><Text style={styles.text}>{'Stock \nAccounts'}</Text>
+                </TouchableOpacity>
+            </View> */}
         </View>
     );
 }
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
-
-    cards: {
-        backgroundColor: '#fff',
-        padding: 40,
-        margin: 5,
-        marginTop: 30,
-        width: '80%',
-        shadowColor: 'black',
-        shadowOpacity: 0.5,
-        shadowRadius: 10,
-        shadowOffset: {
-            width: 0,
-            height: 5
-        },
-        elevation: 5,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
