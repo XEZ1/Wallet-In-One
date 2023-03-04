af1e2e1c60e5f9394c7a6104c8d5fbf48d7d5142
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2023-03-04 18:18:00 +0000

    Display transaction data instead of json object. Render the view that displays the transaction data to fix errors.

--- reactnative/screens/stocks/StockTransactionData.jsx
+++ reactnative/screens/stocks/StockTransactionData.jsx
@@ -2,7 +2,7 @@ import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { useIsFocused } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 
 export default function TransactionData({ route, navigation }){
     const isFocused = useIsFocused()
@@ -26,12 +26,42 @@ export default function TransactionData({ route, navigation }){
 
 
       console.log(JSON.stringify(data))
-      return(
-        <View>
-            <Text>{JSON.stringify(data)}</Text>
-        </View>
+    return (
+      <ScrollView style={styles.screen}>
+        <Text style={styles.text}>Transaction Data{"\n"}</Text>
+
+        {data ? (
+          <View>
+            <Text style={styles.text}>Name</Text>
+            <Text>{data.name}{"\n"}</Text>
+
+            <Text style={styles.text}>Transaction ID</Text>
+            <Text>{data.investment_transaction_id}{"\n"}</Text>
+
+            <Text style={styles.text}>Stock ID</Text>
+            <Text>{data.stock}{"\n"}</Text>
+
+            <Text style={styles.text}>Amount</Text>
+            <Text>£ {data.amount}{"\n"}</Text>
+
+            <Text style={styles.text}>Quantity</Text>
+            <Text>{data.quantity}{"\n"}</Text>
+
+            <Text style={styles.text}>Price</Text>
+            <Text>£ {data.price}{"\n"}</Text>
+
+            <Text style={styles.text}>Fees</Text>
+            <Text>£ {data.fees}{"\n"}</Text>
+
+            <Text style={styles.text}>Date</Text>
+            <Text>{data.date}{"\n"}</Text>
+          </View>
+        ):(<Text>Loading...</Text>)}
+
+        <Text>{JSON.stringify(data)}</Text>
+      </ScrollView>
+  );
     
-        );
 }
 
 const styles = StyleSheet.create({
@@ -51,5 +81,11 @@ const styles = StyleSheet.create({
   ins_name:{
     color: "white",
     fontSize: 18,
+  },
+  text: {
+    fontWeight: 'bold',
+  },
+  screen: {
+    padding: 20,
   }
 });
