d08b963573f8d7d0c4d4b0bd758421c567603f8b
YUSUF ABDULRAHMAN <yusuf.abdulrahman@kcl.ac.uk>
2023-03-10 13:20:31 +0000

    Adds new graphs and show bank insights on frontend

--- backend/banking/views.py
+++ backend/banking/views.py
@@ -13,6 +13,7 @@ from rest_framework.views import APIView
 from .services import get_institutions, create_requisition, delete_all_requisitions, get_requisitions, get_account_data, total_user_balance, get_institution, update_user_accounts
 from .serializers import URLSerializer, OldAccountSerializer, TransactionSerializer, AccountSerializer, format_money
 from .models import Account, Transaction
+from djmoney.money import Money
 
 
 @api_view(['GET'])
@@ -96,10 +97,10 @@ class TransactionList(generics.ListAPIView):
 
 from itertools import groupby
 
-def group_transactions(transactions, balance, interval='day', format=True):
+def group_transactions(transactions, interval, dict=True):
+
     transactions = transactions.annotate(date=TruncDate('time'))
 
-    # Group transactions by date
     if interval == "day":
         key = lambda x: x.date
     elif interval == "time":
@@ -107,8 +108,36 @@ def group_transactions(transactions, balance, interval='day', format=True):
     elif interval == "month":
         key = lambda x: x.date.replace(day=1)
      
-    groups = groupby(transactions, key=key)
-    daily_transaction_sum = {date: sum(t.amount for t in group) for date, group in groups}
+    groups = groupby(sorted(transactions,key=key,reverse=True), key=key)
+
+    if dict:
+        daily_transaction_sum = {str(date): sum(t.amount for t in group) for date, group in groups}
+        return daily_transaction_sum
+    else:
+        return groups
+
+def bar_data(transactions, interval):
+    groups = group_transactions(transactions, interval, False)
+    labels = []
+    values = []
+    for date, group in groups:
+        if interval == 'day':
+            labels.append(date.strftime("%d"))
+        else:
+            labels.append(date.strftime("%B"))
+        values.append(sum(t.amount for t in group).amount)
+
+    labels.reverse()
+    values.reverse()
+
+    data = group_transactions(transactions, interval)
+    data = {date: amount.amount for date, amount in data.items()}
+    return {'labels':labels,'values':values,'data':data}
+
+def calculate_balance_history(transactions, balance, interval='day', format=True):
+
+    # Group transactions by date
+    daily_transaction_sum = group_transactions(transactions, interval)
     dates = sorted(daily_transaction_sum,reverse=True)
 
     daily_balances = {}
@@ -126,39 +155,45 @@ class TransactionChartView(APIView):
     def get(self, request):
         balance = total_user_balance(request.user)
         transactions = Transaction.objects.filter(account__user = self.request.user)
-        daily_balances = group_transactions(transactions, balance, interval='day', format=True)
+        daily_balances = calculate_balance_history(transactions, balance, interval='day', format=True)
         return Response(daily_balances)
 
 from dateutil.relativedelta import relativedelta
 from django.db.models import Max, Min, StdDev, Avg, Variance, Sum
 
-def calculate_metrics(transactions):
+def calculate_metrics(transactions, bar_interval='month'):
+    res= {}
+
+    res['total_amount_of_transactions'] = len(transactions)
+    res['highest_transaction'] = transactions.aggregate(Max('amount')).get('amount__max') or 0
+    res['lowest_transaction'] = transactions.aggregate(Min('amount')).get('amount__min') or 0
+    res['average_transaction'] = transactions.aggregate(Avg('amount')).get('amount__avg') or 0
+    res['variance'] = transactions.aggregate(Variance('amount')).get('amount__variance') or 0
+    res['standard_deviation'] = transactions.aggregate(StdDev('amount')).get('amount__stddev') or 0
+
+    res['bar_data']= bar_data(transactions, interval=bar_interval)
+
+    res['net'] = transactions.aggregate(Sum('amount')).get('amount__sum')
+    return res
+    
+def calculate_metrics_all(transactions,balance,bar_interval='month'):
     res= {}
     positive = transactions.filter(amount__gt=0)
     negative = transactions.filter(amount__lt=0)
 
-    res['total_amount_of_transactions'] = len(transactions)
-    res['highest_transaction'] = transactions.aggregate(Max('amount')).get('amount__max')
-    res['lowest_transaction'] = transactions.aggregate(Min('amount')).get('amount__min')
-    res['average_transaction'] = transactions.aggregate(Avg('amount')).get('amount__avg')
-    res['average_transaction_in'] = positive.aggregate(Avg('amount')).get('amount__avg')
-    res['average_transaction_out'] = negative.aggregate(Avg('amount')).get('amount__avg')
-    res['variance'] = transactions.aggregate(Variance('amount')).get('amount__variance')
-    res['standard_deviation'] = transactions.aggregate(StdDev('amount')).get('amount__stddev')
+    res['positive'] = calculate_metrics(positive, bar_interval)
+    res['negative'] = calculate_metrics(negative, bar_interval)
+    res['both'] = calculate_metrics(transactions, bar_interval)
 
-    balance = Account.objects.all()[2].account_balance()
-    balance_history= group_transactions(transactions, balance, interval='time', format=False)
+    balance_history= calculate_balance_history(transactions, balance, interval='day', format=False)
+    
+    res['balance_history'] = calculate_balance_history(transactions, balance, interval='day', format=False)
+    res['highest_balance'] = max(balance_history.items(), key=lambda x: x[1], default=('',0))[1]
+    res['lowest_balance'] = min(balance_history.items(), key=lambda x: x[1], default=('',0))[1]
 
-    #res['balance_history'] = balance_history
-    res['highest_balance'] = max(balance_history.items(), key=lambda x: x[1], default=0)
-    res['lowest_balance'] = min(balance_history.items(), key=lambda x: x[1], default=0)
-
-    res['debit_transactions'] = len(positive)
-    res['credit_transactions'] = len(negative)
-
-    res['total_money_in'] = positive.aggregate(Sum('amount')).get('amount__sum')
-    res['total_money_out'] = negative.aggregate(Sum('amount')).get('amount__sum')
-    res['net'] = transactions.aggregate(Sum('amount')).get('amount__sum')
+    res['total_money_in'] = positive.aggregate(Sum('amount')).get('amount__sum') or 0
+    res['total_money_out'] = negative.aggregate(Sum('amount')).get('amount__sum') or 0
+    res['net'] = transactions.aggregate(Sum('amount')).get('amount__sum',0)
     return res
 
 @api_view(['GET'])
@@ -179,15 +214,19 @@ def metrics(request, account_id=None):
     res['6 month start'] = start6month
     # # Start of month 12 months ago
     res['12 month start'] = start12month
-    # transactions = Transaction.objects.filter(account__user = request.user, time__gte=start )
-    
-    transactions = Transaction.objects.filter(account=Account.objects.all()[2], account__user = request.user, amount_currency="GBP")
 
-    res['all'] = calculate_metrics(transactions)
-    #res['1month'] = calculate_metrics(transactions.filter(time__gte=start1month))
-    res['3month'] = calculate_metrics(transactions.filter(time__gte=start3month))
-    res['6month'] = calculate_metrics(transactions.filter(time__gte=start6month))
-    return Response(res)
+    #TODO: replace
+    transactions = Transaction.objects.filter(account__user = request.user, amount_currency="GBP")
+    #transactions = Transaction.objects.filter(account=Account.objects.all()[2], account__user = request.user, amount_currency="GBP")
+
+    #balance = Account.objects.all()[2].account_balance() #TODO :remove
+    balance = total_user_balance(request.user)
+
+    res['all'] = calculate_metrics_all(transactions,balance)
+    res['1month'] = calculate_metrics_all(transactions.filter(time__gte=start1month),balance,'day')
+    res['3month'] = calculate_metrics_all(transactions.filter(time__gte=start3month),balance)
+    res['6month'] = calculate_metrics_all(transactions.filter(time__gte=start6month),balance)
+    return Response(res)    
     
 
 @api_view(['GET'])
--- reactnative/screens/banking/BankInsights.js
+++ reactnative/screens/banking/BankInsights.js
@@ -1,39 +1,173 @@
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text, StyleSheet, Button } from 'react-native';
 import { useTheme } from 'reactnative/src/theme/ThemeProvider'
 import { useState, useEffect } from 'react';
 import { useIsFocused } from '@react-navigation/native';
 import Loading from './Loading'
 import { auth_get } from '../../authentication'
+import { BalanceChart } from './BalanceChart'
+import { BankBarChart } from './BankBarChart'
+
+import { BankBarChart2 } from './BankBarChart'
+
+const SegmentedControl = ({ segments, activeIndex, setActiveIndex }) => {
+  return (
+    <View style={styles.segmentContainer}>
+      {segments.map((segment, index) => (
+        <View style={styles.buttonContainer} key={index}>
+          <Button
+            title={segment}
+            color={activeIndex === index ? '#007AFF' : 'grey'}
+            onPress={() => setActiveIndex(index)}
+          />
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  segmentContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    //backgroundColor: '#f2f2f2',
+    borderRadius: 4,
+    overflow: 'hidden',
+  },
+  buttonContainer:{
+    marginHorizontal: 10,
+  },
+  columnCenter:{
+    flexDirection: 'column', alignItems: 'center'
+  },
+  item:{
+    marginVertical:10,
+  }
+});
+
 
 export default function BankInsights() {
   const [ isLoading, setIsLoading ] = useState(true)
   const isFocused = useIsFocused();
-  const {dark, colors, setScheme} = useTheme();
-  const [ data, setData ] = useState('None')
+  const { dark, colors, setScheme} = useTheme();
 
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeTab, setActiveTab ] = useState(0)
+
+  const [ data, setData ] = useState('None')
+  const [ currentData, setCurrentData ] = useState()
+  const [ currentTabData, setCurrentTabData ] = useState()
+  
   useEffect(() =>{
     const fetchData = async () => {
       response = await auth_get('/banking/metrics/')
         
         if (response.status == 200){
             setData(response.body)
+            setCurrentData(response.body.all)
+            setCurrentTabData(response.body.all.both)
+            setActiveIndex(0)
+            setActiveTab(0)
             setIsLoading(false)
+            console.log('check this',response.body.all.balance_history)
         }
     }
     if (isFocused){fetchData()}
   }, [isFocused])
+
+
+  useEffect(() =>{
+    console.log(activeIndex)
+    switch(activeIndex){
+      case 0:
+        setCurrentData(data['all'])
+        break
+      case 1:
+        setCurrentData(data['1month'])
+        break
+      case 2:
+        setCurrentData(data['3month'])
+        break
+      case 3:
+        setCurrentData(data['6month'])
+        break
+    }
+  }, [activeIndex])
+
+  useEffect(() =>{
+    if(currentData){
+      switch(activeTab){
+        case 0:
+          setCurrentTabData(currentData['both'])
+          break
+        case 1:
+          setCurrentTabData(currentData['positive'])
+          break
+        case 2:
+          setCurrentTabData(currentData['negative'])
+          break
+      }
+    }
+  }, [activeTab, currentData])
 
   if (isLoading){
     return <Loading/>
   }
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: colors.background }}>
-        {Object.keys(data.all).map((key) => (
-        <View key={key}>
-          <Text>{key}: {data.all[key]}</Text>
+    <ScrollView>
+    <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: colors.background, marginBottom: 100 }}>
+      <View style={styles.item}>
+        <SegmentedControl segments={['All', '1 Month','3 Month', '6 Month']} activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+      </View>
+      
+      <View style={styles.item}>
+        <View style={{ width: "80%", flexDirection: 'row', justifyContent: 'space-evenly' }}>
+        <View style={styles.columnCenter}>
+            <Text style={{ fontWeight: 'bold' }}>Money In:</Text>
+            <Text style={{ color: 'green' }} >£{currentData.total_money_in}</Text>
+          </View>
+          <View style={styles.columnCenter}>
+            <Text style={{ fontWeight: 'bold' }}>Money Out:</Text>
+            <Text style={{ color: 'red' }} >£{currentData.total_money_out}</Text>
+          </View>
+          <View style={styles.columnCenter}>
+            <Text style={{ fontWeight: 'bold' }}>Net:</Text>
+            <Text style={{ color: currentData.net>0?'green':'red' }} >£{currentData.net}</Text>
+          </View>
         </View>
-      ))}
+      </View>
+      <Text>Balance History:</Text>
+      <BalanceChart rawData={currentData.balance_history} highest={currentData.highest_balance}/>
+      {Object.keys(currentData).map((key) => {
+        if (!["balance_history",'net','total_money_in','total_money_out','positive','negative','both','bar_data'].includes(key)){
+          return (
+            <View key={key}>
+              <Text>{key.replace('_', ' ').replace(/\b(\w)/g, k => k.toUpperCase())}: {currentData[key]}</Text>
+            </View>
+          )
+        }
+      })}
+      <View style={styles.item}>
+        <SegmentedControl segments={['Both', 'Income','Spending']} activeIndex={activeTab} setActiveIndex={setActiveTab} />
+      </View>
+
+      {Object.keys(currentTabData).map((key) => {
+        if (!["balance_history",'net','total_money_in','total_money_out','positive','negative','both','bar_data'].includes(key)){
+          return (
+            <View key={key}>
+              <Text>{key.replace('_', ' ').replace(/\b(\w)/g, k => k.toUpperCase())}: {currentTabData[key]}</Text>
+            </View>
+          )
+        }
+      })}
+
+      
+      <BankBarChart rawData={currentTabData.bar_data} tab={activeTab}/>
+      
+
+
     </View>
+    </ScrollView>
   );
 }
