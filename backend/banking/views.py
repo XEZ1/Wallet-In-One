dd292846fd56669b13b05818b67af4f39312dd96
YUSUF ABDULRAHMAN <yusuf.abdulrahman@kcl.ac.uk>
2023-03-13 15:29:33 +0000

    Refactor banking code by moving calculation functions to separate file and fix bug when querying balance for database

--- backend/banking/views.py
+++ backend/banking/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.utils import timezone
 from django.db.models import Sum
-from django.db.models.functions import TruncDate
 
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
@@ -10,9 +9,11 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.exceptions import APIException
 from rest_framework.views import APIView
+
 from .services import get_institutions, create_requisition, delete_all_requisitions, get_requisitions, get_account_data, total_user_balance, get_institution, update_user_accounts
 from .serializers import URLSerializer, OldAccountSerializer, TransactionSerializer, AccountSerializer, format_money
 from .models import Account, Transaction
+from .util import calculate_balance_history, calculate_metrics_all
 from djmoney.money import Money
 
 
@@ -94,63 +95,6 @@ class TransactionList(generics.ListAPIView):
         else:
             return Transaction.objects.filter(account__user = self.request.user).order_by('-time')
 
-
-from itertools import groupby
-
-def group_transactions(transactions, interval, dict=True):
-
-    transactions = transactions.annotate(date=TruncDate('time'))
-
-    if interval == "day":
-        key = lambda x: x.date
-    elif interval == "time":
-        key = lambda x: x.time
-    elif interval == "month":
-        key = lambda x: x.date.replace(day=1)
-     
-    groups = groupby(sorted(transactions,key=key,reverse=True), key=key)
-
-    if dict:
-        daily_transaction_sum = {str(date): sum(t.amount for t in group) for date, group in groups}
-        return daily_transaction_sum
-    else:
-        return groups
-
-def bar_data(transactions, interval):
-    groups = group_transactions(transactions, interval, False)
-    labels = []
-    values = []
-    for date, group in groups:
-        if interval == 'day':
-            labels.append(date.strftime("%d"))
-        else:
-            labels.append(date.strftime("%B"))
-        values.append(sum(t.amount for t in group).amount)
-
-    labels.reverse()
-    values.reverse()
-
-    data = group_transactions(transactions, interval)
-    data = {date: amount.amount for date, amount in data.items()}
-    return {'labels':labels,'values':values,'data':data}
-
-def calculate_balance_history(transactions, balance, interval='day', format=True):
-
-    # Group transactions by date
-    daily_transaction_sum = group_transactions(transactions, interval)
-    dates = sorted(daily_transaction_sum,reverse=True)
-
-    daily_balances = {}
-    for d in dates:
-        if format:
-            daily_balances[str(d)] = str(balance)
-        else:
-            daily_balances[str(d)] = balance.amount
-        balance -= daily_transaction_sum[d]
-    
-    return daily_balances
-
-
 class TransactionChartView(APIView):
     def get(self, request):
         balance = total_user_balance(request.user)
@@ -159,42 +103,6 @@ class TransactionChartView(APIView):
         return Response(daily_balances)
 
 from dateutil.relativedelta import relativedelta
-from django.db.models import Max, Min, StdDev, Avg, Variance, Sum
-
-def calculate_metrics(transactions, bar_interval='month'):
-    res= {}
-
-    res['total_amount_of_transactions'] = len(transactions)
-    res['highest_transaction'] = transactions.aggregate(Max('amount')).get('amount__max') or 0
-    res['lowest_transaction'] = transactions.aggregate(Min('amount')).get('amount__min') or 0
-    res['average_transaction'] = transactions.aggregate(Avg('amount')).get('amount__avg') or 0
-    res['variance'] = transactions.aggregate(Variance('amount')).get('amount__variance') or 0
-    res['standard_deviation'] = transactions.aggregate(StdDev('amount')).get('amount__stddev') or 0
-
-    res['bar_data']= bar_data(transactions, interval=bar_interval)
-
-    res['net'] = transactions.aggregate(Sum('amount')).get('amount__sum')
-    return res
-    
-def calculate_metrics_all(transactions,balance,bar_interval='month'):
-    res= {}
-    positive = transactions.filter(amount__gt=0)
-    negative = transactions.filter(amount__lt=0)
-
-    res['positive'] = calculate_metrics(positive, bar_interval)
-    res['negative'] = calculate_metrics(negative, bar_interval)
-    res['both'] = calculate_metrics(transactions, bar_interval)
-
-    balance_history= calculate_balance_history(transactions, balance, interval='day', format=False)
-    
-    res['balance_history'] = calculate_balance_history(transactions, balance, interval='day', format=False)
-    res['highest_balance'] = max(balance_history.items(), key=lambda x: x[1], default=('',0))[1]
-    res['lowest_balance'] = min(balance_history.items(), key=lambda x: x[1], default=('',0))[1]
-
-    res['total_money_in'] = positive.aggregate(Sum('amount')).get('amount__sum') or 0
-    res['total_money_out'] = negative.aggregate(Sum('amount')).get('amount__sum') or 0
-    res['net'] = transactions.aggregate(Sum('amount')).get('amount__sum',0)
-    return res
 
 @api_view(['GET'])
 def metrics(request, account_id=None):
