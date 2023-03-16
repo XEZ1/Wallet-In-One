fa9b915daba5c203ec0d76da02ea15e40b0535ad
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-03-16 18:50:56 +0000

    Test crypto_exchanges.services.ExchangeFetcher

--- backend/crypto_exchanges/tests/Services/test_services.py
+++ backend/crypto_exchanges/tests/Services/test_services.py
@@ -4,47 +4,85 @@ from unittest.mock import patch, MagicMock
 from crypto_exchanges.services import *
 
 
-class TestBinanceFetcher(TestCase):
-
+class TestGenericExchangeFetcher(TestCase):
     def setUp(self):
-        self.api_key = "test_api_key"
-        self.secret_key = "test_secret_key"
-        self.fetcher = BinanceFetcher(api_key=self.api_key, secret_key=self.secret_key)
+        self.api_key = ""
+        self.secret_key = ""
+        self.fetcher = ExchangeFetcher(api_key=self.api_key, secret_key=self.secret_key)
 
-    @patch("crypto_exchanges.fetchers.time")
-    def test_get_current_time(self, mock_time):
-        mock_time.time.return_value = 1615256702.123456
-        self.assertEqual(self.fetcher.get_current_time(), 1615256702123)
+    def test_get_current_time(self):
+        # Test that the method returns an integer with a right value
+        current_time = self.fetcher.get_current_time()
+        self.assertIsInstance(current_time, int)
+        self.assertAlmostEquals(current_time, round(time.time() * 1000))
 
     def test_prehash(self):
-        timestamp = "1615256702123"
-        method = "GET"
-        path = "/api/v3/account"
+        # Test that the method returns a string with a right value
+        timestamp = str(int(time.time()))
+        method = 'GET'
+        path = '/v1/hello-world/'
         body = None
-        expected = "1615256702123GET/api/v3/account"
-        self.assertEqual(self.fetcher.prehash(timestamp, method, path, body), expected)
+        prehash = self.fetcher.prehash(timestamp=timestamp, method=method, path=path, body=body)
+        self.assertIsInstance(prehash, str)
+        self.assertEquals(prehash, timestamp + method.upper() + path + (body or ''))
 
     def test_hash(self):
-        timestamp = "1615256702123"
-        expected = "7e0a5858c5d9e5d087e5e5dbd6478d5d5f5c5fb2b1dbf73a72e7d319c32ef13"
-        self.assertEqual(self.fetcher.hash(timestamp), expected)
+        # Test that the method returns a string with a right value
+        timestamp = str(int(time.time()))
+        hashed = self.fetcher.hash(timestamp=timestamp)
+        self.assertIsInstance(hashed, str)
+        self.assertEquals(hashed, hmac.new(self.secret_key.encode('utf-8'), timestamp.encode('utf-8'), sha256).hexdigest())
 
-    @patch("crypto_exchanges.fetchers.requests.get")
-    def test_get_account_data(self, mock_get):
-        expected = {"balances": [{"asset": "BTC", "free": "0.01", "locked": "0.00"}, {"asset": "ETH", "free": "0.10",
-                                                                                      "locked": "0.00"}]}
-        mock_response = MagicMock()
-        mock_response.json.return_value = expected
-        mock_get.return_value = mock_response
+    def test_get_account_data(self):
+        # Test that the method raises an error
+        with self.assertRaises(NotImplementedError):
+            self.fetcher.get_account_data()
 
-        actual = self.fetcher.get_account_data()
+    def test_get_trading_history(self):
+        # Test that the method raises an error
+        with self.assertRaises(NotImplementedError):
+            self.fetcher.get_trading_history()
 
-        mock_get.assert_called_once_with(url="https://api.binance.com/api/v3/account?timestamp=1615256702123&signature="
-        "7e0a5858c5d9e5d087e5e5dbd6478d5d5f5c5fb2b1dbf73a72e7d319c32ef13", headers={'X-MBX-APIKEY': self.api_key})
-        self.assertEqual(actual, expected)
-
-    @patch("crypto_exchanges.fetchers.requests.get")
-    def test_get_trading_history(self, mock_get):
-        expected = {
-            "BTCUSDT": [{"id": 12345, "price": "56000", "qty": "0.1", "commission": "0.05"}, {"id": 67890, "price": "58000", "qty": "0.05", "commission": "0.025"}],
-            "ETHUSDT": [{"id": 123, "price": "1800", "qty": "1.0", "commission": "0.01"}, {"id": 456, "price": "1900", "qty": "0.5", "commission": "0.005"}],
+#
+    #@patch('requests.get')
+    #def test_get_account_data(self, mock_get):
+    #    # Test that the method sends a GET request with the correct parameters
+    #    mock_response = MagicMock()
+    #    mock_response.json.return_value = {'test': 'data'}
+    #    mock_get.return_value = mock_response
+#
+    #    expected_url = "https://api.binance.com/api/v3/account"
+    #    expected_headers = {'X-MBX-APIKEY': self.api_key}
+    #    expected_timestamp = str(self.fetcher.get_current_time())
+    #    expected_signature = self.fetcher.hash(timestamp=f"timestamp={expected_timestamp}")
+    #    expected_params = {'timestamp': expected_timestamp, 'signature': expected_signature}
+#
+    #    self.fetcher.get_account_data()
+#
+    #    mock_get.assert_called_with(url=expected_url, headers=expected_headers, params=expected_params)
+#
+    #@patch('requests.get')
+    #def test_get_trading_history(self, mock_get):
+    #    # Test that the method sends a GET request with the correct parameters for each symbol
+    #    mock_response = MagicMock()
+    #    mock_response.json.return_value = {'test': 'data'}
+    #    mock_get.return_value = mock_response
+#
+    #    expected_url = "https://api.binance.com/api/v3/myTrades"
+    #    expected_headers = {'X-MBX-APIKEY': self.api_key}
+    #    expected_timestamp = str(self.fetcher.get_current_time())
+#
+    #    expected_params_btcusdt = {'symbol': 'BTCUSDT', 'timestamp': expected_timestamp}
+    #    expected_signature_btcusdt = self.fetcher.signature(expected_params_btcusdt)
+    #    expected_params_btcusdt.update({'signature': expected_signature_btcusdt})
+#
+    #    expected_params_ethusdt = {'symbol': 'ETHUSDT', 'timestamp': expected_timestamp}
+    #    expected_signature_ethusdt = self.fetcher.signature(expected_params_ethusdt)
+    #    expected_params_ethusdt.update({'signature': expected_signature_ethusdt})
+#
+    #    expected_params_adausdt = {'symbol': 'ADAUSDT', 'timestamp': expected_timestamp}
+    #    expected_signature_adausdt = self.fetcher.signature(expected_params_adausdt)
+    #    expected_params_adausdt.update({'signature': expected_signature_adausdt})
+#
+    #    expected_params_xrpusdt = {'symbol': 'XRPUSDT', 'timestamp': expected_timestamp}
+    #    expected_signature_xrpusdt = self.fetcher.signature(expected_params_xrpusdt)
