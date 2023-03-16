80185dc470ef99b399b642ba83de3ea3c65a3ee2
krishna <krishnaprasannakumar21@gmail.com>
2023-03-16 13:36:44 +0000

    Add Metrics View Tests

--- backend/stocks/urls.py
+++ backend/stocks/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('add_stock/', addStock.as_view(), name='add_stock'),
     path('list_stocks/<str:stockAccount>/', listStocks, name='list_stocks'),
     path('get_logo/', get_logo, name='get_logo'),
-    path('get_metrics/', getMetrics),
+    path('get_metrics/', getMetrics, name='get_stock_metrics'),
     path('get_account/<str:account_id>/', getAccount, name='get_stock_account')
 ]
