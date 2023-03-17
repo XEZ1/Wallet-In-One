81cddeb1bccdeecc1e3fbd6eaff1b5871b8d9cdc
krishna <krishnaprasannakumar21@gmail.com>
2023-03-17 22:40:02 +0000

    Update Bar Chart Tests

--- reactnative/tests/charts/BarChart.test.js
+++ reactnative/tests/charts/BarChart.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BarChart from '../../screens/charts/chartComponents/barChart';
 import renderer from 'react-test-renderer';
+import { StyleSheet } from 'react-native';
 
 var data = {
     "all": [
@@ -48,30 +49,30 @@ var emptyData = {"all": []}
 const emptyArray = Object.values(emptyData)[0]
 const array = Object.values(data)[0]
 const floatArray = Object.values(floatData)[0]
-var colours = ["pink", "turquoise", "lime", "#FA991C"]
+var colours = ["pink", "turquoise", "lime", "#FA991C"];
 const handlePressIn = (event, datapoint)=>{console.log(datapoint)};
 const list = array.map((val) => val.x);
 const floatList = floatArray.map((val) => val.x);
-const colors = {"text": "black"}
+const colors = { text: 'black' };
 
 describe('<BarChart />', () => {
     it('test bar chart', () => {
-        const barChart = renderer.create(<BarChart colours={colours} list={list} data={data} handlePressIn={handlePressIn} spacing={list.length*60} colors={colors} />);
+        const barChart = renderer.create(BarChart(colours, list, data.all, list.length*60, handlePressIn, colors));
         expect(barChart).toMatchSnapshot();         
       });
 
     it('test bar chart with float values', () => {
-        const barChart = renderer.create(<BarChart colours={colours} list={floatList} data={floatData} handlePressIn={handlePressIn} spacing={list.length*60} colors={colors} />);
+        const barChart = renderer.create(BarChart(colours, floatList, floatData.all, floatList.length*60, handlePressIn, colors));
         expect(barChart).toMatchSnapshot();         
       });
 
     it('test bar chart with empty data', () => {
-      const barChart = renderer.create(<BarChart colours={colours} list={emptyArray} data={emptyData} handlePressIn={handlePressIn} spacing={list.length*60} colors={colors} />);
+      const barChart = renderer.create(BarChart(colours, emptyArray, emptyData.all, emptyArray.length*60, handlePressIn, colors));
       expect(barChart).toMatchSnapshot();     
       });
 
     it('test bar chart no props', () => {
-        const barChart = renderer.create(<BarChart />).toJSON();
+        const barChart = renderer.create(BarChart()).toJSON();
         expect(barChart).toBeNull();
         expect(barChart).toMatchSnapshot();
       });
