9872f9065a5984c9c957a0b293fa407e59003acb
Abbas Bin Vakas <k21013731@kcl.ac.uk>
2023-03-18 18:29:53 +0000

    Broken graph data view test with banking fixture imports

--- backend/accounts/tests/Views/testGraphData_view.py
+++ backend/accounts/tests/Views/testGraphData_view.py
@@ -5,13 +5,16 @@ from rest_framework.test import APIClient, APIRequestFactory
 from django.urls import reverse
 from accounts.views import graph_data
 from accounts.models import User
+from banking.models import Account, Transaction
 from rest_framework import status
+from django.utils import timezone
 
 class GraphDataViewTestCase(TestCase):
     """Tests for the graph data view."""
 
     fixtures = [
         'accounts/fixtures/user.json',
+        'banking/tests/fixtures/bank_data.json',
     ]
 
     def setUp(self):
@@ -25,24 +28,23 @@ class GraphDataViewTestCase(TestCase):
         self.assertEqual(self.url,'/graph_data/')
 
     def test_no_graph_data(self):
-        APIRequestFactory().post('/')
-        request = self.factory.get('/graph_data/')
+        request = self.factory.get(self.url)
         request.user = self.user
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['all']), 0)
 
-    # def test_bank_connected(self):
-    #     APIRequestFactory().post('/')
-    #     request = self.factory.get('/graph_data/')
-    #     request.user = self.user
-    #     response = self.client.get(self.url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data['all'][0]['x'], 'Banks')
-    #     self.assertEqual(response.data['all'][0]['y'], 0)
-    #     # self.assertEqual(response.data['all'][1]['x'], 'Cryptocurrency from wallets')
-    #     # self.assertEqual(response.data['all'][1]['y'], 0)
-    #     # self.assertEqual(response.data['all'][2]['x'], 'Stock Accounts')
-    #     # self.assertEqual(response.data['all'][2]['y'], 0)
-    #     # self.assertEqual(response.data['all'][3]['x'], 'Cryptocurrency from exchanges')
-    #     # self.assertEqual(response.data['all'][3]['y'], 0)
+    def test_bank_connected(self):
+        request = self.factory.get(self.url)
+        request.user = self.user
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # self.assertEqual(response.data['all'][0]['x'], 'Banks')
+        # self.assertEqual(response.data['all'][0]['y'], 0)
+        # self.assertEqual(response.data['all'][1]['x'], 'Cryptocurrency from wallets')
+        # self.assertEqual(response.data['all'][1]['y'], 0)
+        # self.assertEqual(response.data['all'][2]['x'], 'Stock Accounts')
+        # self.assertEqual(response.data['all'][2]['y'], 0)
+        # self.assertEqual(response.data['all'][3]['x'], 'Cryptocurrency from exchanges')
+        # self.assertEqual(response.data['all'][3]['y'], 0)
