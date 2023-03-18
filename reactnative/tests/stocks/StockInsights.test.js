5b64073ef51f0d15ebf85c0fdbf5d566735fc502
krishna <krishnaprasannakumar21@gmail.com>
2023-03-18 14:31:20 +0000

    Improve Stock Insights Screen Tests

--- reactnative/tests/stocks/StockInsights.test.js
+++ reactnative/tests/stocks/StockInsights.test.js
@@ -21,7 +21,7 @@ describe('<StockInsight />', () => {
     }
     )
 
-    it('test stock insight switch', async () => {
+    it('test 1 month stock insight', async () => {
 
         const stockInsight = render(<StockInsight/>);
 
@@ -38,6 +38,59 @@ describe('<StockInsight />', () => {
             expect(screen.getByText("Lowest Transaction(£): -12418.45"))
             expect(screen.getByText("Average Transaction(£): -1218.68"))
             expect(screen.getByText("Variance: 10783023.6"))
+            expect(screen.getByText("Standard Deviation: 3283.75"))
+            expect(screen.getByText("Highest Fee(£): 7.99"))
+            expect(screen.getByText("Lowest Fee(£): 0"))
+            expect(screen.getByText("Average Fee(£): 3.07"))
+        });
+        expect(stockInsight).toMatchSnapshot();
+    }
+    )
+
+    it('test 3 months stock insight', async () => {
+
+        const stockInsight = render(<StockInsight/>);
+
+        await act( async () => {
+            
+            await waitFor( () => {
+                const activityIndicator = screen.UNSAFE_queryByType('ActivityIndicator');
+                expect(activityIndicator).toBeNull();
+            })
+
+            fireEvent.press(await screen.getByTestId('3 Months'));
+            expect(screen.getByText("Number of Transactions: 39"))
+            expect(screen.getByText("Highest Transaction(£): 387.37"))
+            expect(screen.getByText("Lowest Transaction(£): -12418.45"))
+            expect(screen.getByText("Average Transaction(£): -1218.68"))
+            expect(screen.getByText("Variance: 10783023.6"))
+            expect(screen.getByText("Standard Deviation: 3283.75"))
+            expect(screen.getByText("Highest Fee(£): 7.99"))
+            expect(screen.getByText("Lowest Fee(£): 0"))
+            expect(screen.getByText("Average Fee(£): 3.07"))
+        });
+        expect(stockInsight).toMatchSnapshot();
+    }
+    )
+
+    it('test 6 months stock insight', async () => {
+
+        const stockInsight = render(<StockInsight/>);
+
+        await act( async () => {
+            
+            await waitFor( () => {
+                const activityIndicator = screen.UNSAFE_queryByType('ActivityIndicator');
+                expect(activityIndicator).toBeNull();
+            })
+
+            fireEvent.press(await screen.getByTestId('6 Months'));
+            expect(screen.getByText("Number of Transactions: 78"))
+            expect(screen.getByText("Highest Transaction(£): 387.37"))
+            expect(screen.getByText("Lowest Transaction(£): -12418.45"))
+            expect(screen.getByText("Average Transaction(£): -1218.68"))
+            expect(screen.getByText("Variance: 10783023.6"))
+            expect(screen.getByText("Standard Deviation: 3283.75"))
             expect(screen.getByText("Highest Fee(£): 7.99"))
             expect(screen.getByText("Lowest Fee(£): 0"))
             expect(screen.getByText("Average Fee(£): 3.07"))
