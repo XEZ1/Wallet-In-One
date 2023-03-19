af2d6edb5b78ec27ffe619eab6b3f2b90dbdbb1d
Matushan Yogaraj <dev@matushan.com>
2023-03-19 22:23:09 +0000

    Create tests for CryptoWalletDetail

--- reactnative/tests/mock.js
+++ reactnative/tests/mock.js
@@ -2,3 +2,7 @@ jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('@react-navigation/native', () => ({
     useIsFocused: jest.fn(),
   }));
+
+jest.mock('react-native-reanimated', () => require('react-native-reanimated/mock'))
+
+jest.mock('react-native-wagmi-charts')
