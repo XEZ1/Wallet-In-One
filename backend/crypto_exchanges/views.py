557e3fff16f7a850769f2038d4d4accbd75e0c3e
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-03-20 18:26:32 +0000

    Fix the referesh button

--- backend/crypto_exchanges/views.py
+++ backend/crypto_exchanges/views.py
@@ -473,6 +473,8 @@ class KrakenView(GenericCryptoExchanges, ABC):
 class UpdateAllTokens(APIView):
     def post(self, request):
         fixed_accounts = CryptoExchangeAccount.objects.filter(user=request.user)
+        if len(fixed_accounts) == 0:
+            return Response({'message': 'There are no accounts connected'}, status=204)
         for account in fixed_accounts:
             api_key = account.api_key
             secret_key = account.secret_key
