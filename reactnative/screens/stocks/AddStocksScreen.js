4b62224b51b5d249e727bc576ac68006c278a390
krishna <krishnaprasannakumar21@gmail.com>
2023-03-22 18:58:21 +0000

    Refactor Code

--- reactnative/screens/stocks/AddStocksScreen.js
+++ reactnative/screens/stocks/AddStocksScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect,useRef  } from 'react';
-import { Image ,Dimensions } from 'react-native';
+import { Image } from 'react-native';
 import PlaidLink from '@burstware/expo-plaid-link'
 import { useIsFocused } from '@react-navigation/native';
 import { auth_post } from '../../authentication';
@@ -8,10 +8,7 @@ import AddStocksHelper from './AddStocksHelper';
 import { styles } from "reactnative/screens/All_Styles.style.js";
 import ConditionalModal from '../Modal';
 
-import {Alert, Modal, StyleSheet, Pressable, View, Animated} from 'react-native';
-
-const { width, height } = Dimensions.get('window');
-
+import { StyleSheet, Pressable, View, Animated} from 'react-native';
 
 const PlaidComponent = ({ navigation }) => {
   const [linkToken, setLinkToken] = useState('');
@@ -61,22 +58,6 @@ const PlaidComponent = ({ navigation }) => {
     },
   });
 
-  const account_data = null;
-
-  // const addAccount = async (account, success) => {
-  //   const account_data = {
-  //     account_id: account._id,
-  //     name: account.meta.name,
-  //     institution_name: success.metadata.institution.name,
-  //     institution_id: success.metadata.institution.id,
-  //     access_token: access_token,
-  //     balance: balance,
-  //     institution_logo: image,
-  //   }
-  //   const response = await auth_post('/stocks/add_stock_account/', account_data)
-  //   data_response = response.status
-  // };
-
   useEffect(() => {
   const initiatePlaidLink = async () => {
       const response = await auth_post('/stocks/initiate_plaid_link/')
@@ -86,85 +67,6 @@ const PlaidComponent = ({ navigation }) => {
   };
   if(useIsFocused){initiatePlaidLink()}
 }, [isFocused])
-
-  // const getAccessToken = async (publicToken) => {
-  //   const body = {
-  //     public_token: publicToken
-  //   }
-  //   const response = await auth_post('/stocks/get_access_token/', body)
-  //   access_token = response.body.access_token
-  // }
-
-  // const getBalance = async (accessToken) => {
-  //   const body = {
-  //     access_token: accessToken
-  //   }
-  //   const response = await auth_post('/stocks/get_balance/', body)
-  //   const data = response.body;
-  //   balance = (parseFloat(data.accounts[0].balances.current)*0.83).toFixed(2)
-  // }
-
-  // const getStocks = async (accessToken) => {
-  //   const body = {
-  //     access_token: accessToken
-  //   }
-  //   const response = await auth_post('/stocks/get_stocks/', body)
-  //   const data = response.body;
-  //   stocks = data.holdings
-  //   securities = data.securities
-  // }
-
-  // const addStock = async (stock, stockInfo) => {
-  //   const body = {
-  //     institution_price: (stock.institution_price).toFixed(2),
-  //     quantity: stock.quantity,
-  //     name: stockInfo.name,
-  //     ticker_symbol: stockInfo.ticker_symbol,
-  //     stockAccount: stock.account_id,
-  //     security_id: stockInfo.security_id
-  //   }
-  //   const res = await auth_post('/stocks/add_stock/', body)
-  //   if(res.status == 201){
-  //     setModalVisible(true);
-  //   }
-  // }
-
-  // const getTransaction = async (accessToken) => {
-  //   const body = {
-  //     access_token: accessToken
-  //   }
-  //   const response = await auth_post('/stocks/get_transactions/', body)
-  //   fetched_transaction_list = response.body
-  // }
-
-  // const addTransaction = async (element) => {
-  //   let latitude = parseFloat(((Math.random() * (7) + 35.5).toFixed(3)))
-  //   let longitude = parseFloat(((Math.random() * (43) + 77).toFixed(3))) * -1
-  //   const body = {
-  //     account_id: element.account_id,
-  //     investment_transaction_id: element.investment_transaction_id,
-  //     security_id: element.security_id,
-  //     date: element.date,
-  //     name: element.name,
-  //     quantity: element.quantity,
-  //     amount: element.amount * 0.83, // to convert to GBP
-  //     price: element.price,
-  //     fees: element.fees,
-  //     stock: fetched_transaction_list.accounts[0].account_id,
-  //     latitude: latitude,
-  //     longitude: longitude
-  //   }
-  //   const response = await auth_post('/stocks/add_transaction_account/', body)
-  //   };
-
-
-  // const getLogo = async (success) => {
-  //   const body = {
-  //     name: success.metadata.institution.name
-  //   }
-  //   const response = await auth_post('/stocks/get_logo/', body)
-  //   image = response.body.logo
-  // }
 
   const [modalVisible, setModalVisible] = useState(false);
   const [modalText, setModalText] = useState("Empty Modal");
