37de162331f788765cc57ef8e330eec3d98a2c91
Ezzat Alsalibi (k20047135) <ezzat.alsalibi@kcl.ac.uk>
2023-03-23 17:43:15 +0000

    Fix wrong http response

--- backend/crypto_exchanges/views.py
+++ backend/crypto_exchanges/views.py
@@ -484,7 +484,7 @@ class UpdateAllTokens(APIView):
     def post(self, request):
         fixed_accounts = CryptoExchangeAccount.objects.filter(user=request.user)
         if len(fixed_accounts) == 0:
-            return Response({'message': 'There are no accounts connected'}, status=status.HTTP_204_NO_CONTENT)
+            return Response({'message': 'There are no accounts connected'}, status=status.HTTP_200_OK)
         for account in fixed_accounts:
             api_key = account.api_key
             secret_key = account.secret_key
